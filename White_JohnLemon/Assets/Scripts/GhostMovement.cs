ca0721c12b1b6c4742769e2e18fc147d5621ae3d
cjw20 <54556219+cjw20@users.noreply.github.com>
2020-02-27 15:24:15 -0500

    added moving player ghost

---
+++ White_JohnLemon/Assets/Scripts/GhostMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostMovement : MonoBehaviour
+{
+    public float speed = 10;
+    public float turnSpeed = 20f;
+    Quaternion m_Rotation = Quaternion.identity;
+    private Rigidbody body;
+    Vector3 m_Movement;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");  //Gets input
+
+        m_Movement.Set(horizontal, 0f, vertical);  //Updates movement vector
+       // m_Movement.Normalize();
+        body.AddForce(m_Movement * speed);
+       
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation(desiredForward);
+        body.MoveRotation(m_Rotation);
+
+    }
+}
