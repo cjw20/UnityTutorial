374b9145870d67b8b1932deb717d7af143b738ff
cjw20 <54556219+cjw20@users.noreply.github.com>
2020-03-03 11:17:52 -0500

    edited waypoint script to make john run from player

--- White_JohnLemon/Assets/Scripts/WaypointPatrol.cs
+++ White_JohnLemon/Assets/Scripts/WaypointPatrol.cs
@@ -7,6 +7,7 @@ public class WaypointPatrol : MonoBehaviour
 {
     public NavMeshAgent navMeshAgent;
     public Transform[] waypoints;
+    public Transform player;
     int m_CurrentWaypointIndex;
     // Start is called before the first frame update
     void Start()
@@ -17,10 +18,27 @@ public class WaypointPatrol : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
+        float farPoint = 0f; //variable for storing fathest waypoint from player
+        Vector3 target = Vector3.zero;
+
+        foreach (Transform point in waypoints)
         {
-            m_CurrentWaypointIndex = (m_CurrentWaypointIndex + 1) % waypoints.Length;
-            navMeshAgent.SetDestination(waypoints[m_CurrentWaypointIndex].position);
+            float playerDistance = Vector3.Distance(player.position, point.position);
+
+            if(playerDistance > farPoint)
+            {
+                farPoint = playerDistance;
+                target = point.position;
+            }
+
+            
         }
+        navMeshAgent.SetDestination(target);
+
+        // if(navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
+        //{
+        //  m_CurrentWaypointIndex = (m_CurrentWaypointIndex + 1) % waypoints.Length;
+        //navMeshAgent.SetDestination(waypoints[m_CurrentWaypointIndex].position);
+        //   }
     }
 }
