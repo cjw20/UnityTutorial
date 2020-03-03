89ac066626e3444e91b093fc92a0346ab22c77f2
cjw20 <54556219+cjw20@users.noreply.github.com>
2020-03-03 12:10:10 -0500

    started footprint scripts; currently not working properly

---
+++ White_JohnLemon/Assets/Scripts/Printmaker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Printmaker : MonoBehaviour
+{
+    public GameObject Footprint;
+    public GameObject Intruder;
+    public float delay = 1f; //time between footprints
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float timer = delay;
+        timer -= Time.deltaTime;
+        if(timer <= 0)
+        {
+            Instantiate(Footprint, Intruder.transform);
+            timer = delay;
+        }
+    }
+}
